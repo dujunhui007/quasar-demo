1287d0e1ee30c5ebae43ece34cab0f087dd3c86b
dujunhui007 <810743626@qq.com>
2018-11-13 14:31:56 +0800

    chore：Beautify the code

--- src/plugins/element-ui.js
+++ src/plugins/element-ui.js
@@ -1,15 +1,5 @@
-// import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// // import App from './App.vue'
-
-// export default ({
-//   app,
-//   store,
-//   Vue
-// }) => {
-//   Vue.use(ElementUI)
-// }
 
 export default ({
   Vue
